1e5684edcdea3ae97cf96fc00c8c06e18712cbcc
Serhii <5120853+krb3d@users.noreply.github.com>
2023-12-27 15:50:53 +0100

    Feature/chapter-1
    Chapter-1 done

---
+++ chapter-1/react-starter/src/App.jsx
@@ -0,0 +1,13 @@
+
+import DemoComponent from './DemoComponent'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello React</h1>
+      <DemoComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ chapter-1/react-starter/src/DemoComponent.jsx
@@ -0,0 +1,9 @@
+function DemoComponent(){
+    return(
+        <div>
+            <h2>This is an Demo Component</h2>
+        </div>
+    );
+}
+
+export default DemoComponent;
